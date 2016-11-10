2245d4dc78da86e82fc5eb4bf6cb48532b538864
Stuart Winchester <stuart.winchester@gmail.com>
2016-11-10 10:51:51 +0000

    trying out spring boot on plain tomcat container

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,12 +3,19 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	@RequestMapping("/")
 	public String home() {
